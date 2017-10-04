ddeb4562cae2a6bbbd1accd01164d5ad25eea048
Jeremy Downs <jeremy.l.downs@gmail.com>
2017-10-04 14:03:25 -0400

    Cleanup

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,8 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
+def index():
     return render_template("base_nav.html")
-
-# @application.route("/")
-# def hello():
-#    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
